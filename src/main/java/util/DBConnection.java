941fe4b22fb30b3879364d7ec9dbc51f4832267b
abhil2002 <abhisheklawhale2002@gmail.com>
2026-01-29 10:56:27 +0530

    ERD

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/revshop";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_mysql_password";
+    private static final String PASSWORD = "root@123";
 
     public static Connection getConnection() {
         try {
